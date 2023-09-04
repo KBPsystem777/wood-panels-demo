c62c4a91c243080c64169dd60bba23152023eca5
kbpsystem <koleen.bp@outlook.com>
2023-09-05 01:51:19 +0800

    initial commit for mcap woodpanels

--- src/App.js
+++ src/App.js
@@ -1,47 +1,78 @@
-import React from 'react';
-import styled from 'styled-components';
-import Waveform from './waveform';
-import pianoClip from './piano.mp3';
+import React, { useState } from "react"
+import Waveform from "./waveform"
+import pianoClip from "./piano.mp3"
+import reverb from "./reverb.mp3"
+import noReverb from "./noReverb.mp3"
+
+import styled from "styled-components"
+
+const ToggleSwitchContainer = styled.label`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  padding-top: 5em;
+`
+
+const SwitchInput = styled.input`
+  display: none;
+`
+
+const Slider = styled.span`
+  position: relative;
+  width: 40px;
+  height: 20px;
+  background-color: green;
+  border-radius: 20px;
+  transition: background-color 0.3s;
+
+  &::before {
+    content: "";
+    position: absolute;
+    width: 18px;
+    height: 18px;
+    border-radius: 50%;
+    background-color: white;
+    top: 1px;
+    left: 1px;
+    transition: transform 0.3s;
+  }
+`
+
+const Text = styled.span`
+  margin-left: 10px;
+`
 
 const App = () => {
+  const [isReverb, setIsReverb] = useState(!false)
+
   return (
-    <main style={{ padding: '16px' }}>
-      <h1>WaveSurfer Demo</h1>
-      <Waveform audio={pianoClip} />
-      <footer style={{ marginTop: '32px' }}>
-        <p>
-          Made by
-          {' '}
-          <Link href="https://andreidobrinski.com">Andrei Dobrinski</Link>
-        </p>
-        <p>Check out the
-          {' '}
-          <Link href="https://andreidobrinski.com/blog/implementing-an-audio-waveform-with-react-hooks-and-wavesurferjs/">tutorial</Link>
-          {' '}
-          and
-          {' '}
-          <Link href="https://github.com/andreidobrinski/react-wavesurfer-demo/blob/master/src/waveform.js">source code.</Link>
-        </p>
-      </footer>
+    <main style={{ padding: "16px" }}>
+      <h1>WoodPanels Demo</h1>
+      <Waveform audio={isReverb ? reverb : noReverb} />
+
+      {/* <button
+        onClick={() => {
+          console.log("isReverb: ", isReverb)
+          setIsReverb(!isReverb)
+        }}
+      >
+        No reverb
+      </button> */}
+
+      <ToggleSwitchContainer>
+        <SwitchInput
+          type="checkbox"
+          checked={isReverb}
+          onChange={() => {
+            console.log("isReverb: ", isReverb)
+            setIsReverb(!isReverb)
+          }}
+        />
+        <Slider />
+        <Text>{!isReverb ? "No reverb" : "With reverb"}</Text>
+      </ToggleSwitchContainer>
     </main>
-  );
+  )
 }
 
-const Link = styled.a`
-  --primary: #61dbff;
-  color: rgba(0, 0, 0, 0.9);
-  text-decoration: none;
-  padding: 0 1px;
-  box-shadow: inset 0 -6px 0 0 var(--primary);
-  transition: box-shadow 0.3s ease-in;
-
-  &:hover {
-    box-shadow: inset 0 -40px 0 0 var(--primary);
-  }
-
-  &:active {
-    box-shadow: inset 0 -40px 0 0 var(--primary);
-  }
-`;
-
-export default App;
+export default App
--- src/waveform.js
+++ src/waveform.js
@@ -1,40 +1,42 @@
-import React, { useState, useEffect, useRef } from 'react';
-import PropTypes from 'prop-types';
-import WaveSurfer from 'wavesurfer.js';
-import styled from 'styled-components';
-import { FaPlayCircle, FaPauseCircle } from 'react-icons/fa';
+import React, { useState, useEffect, useRef } from "react"
+import PropTypes from "prop-types"
+import WaveSurfer from "wavesurfer.js"
+import styled from "styled-components"
+import { FaPlayCircle, FaPauseCircle } from "react-icons/fa"
 
 const Waveform = ({ audio }) => {
-  const containerRef = useRef();
+  const containerRef = useRef()
   const waveSurferRef = useRef({
     isPlaying: () => false,
-  });
-  const [isPlaying, toggleIsPlaying] = useState(false);
+  })
+  const [isPlaying, toggleIsPlaying] = useState(false)
 
   useEffect(() => {
     const waveSurfer = WaveSurfer.create({
       container: containerRef.current,
       responsive: true,
-      barWidth: 2,
-      barHeight: 10,
+      barWidth: 5,
+      barHeight: 5,
       cursorWidth: 0,
-    });
-    waveSurfer.load(audio);
-    waveSurfer.on('ready', () => {
-      waveSurferRef.current = waveSurfer;
-    });
+      progressColor: "gray",
+      //   maxCanvasWidth: 12,
+    })
+    waveSurfer.load(audio)
+    waveSurfer.on("ready", () => {
+      waveSurferRef.current = waveSurfer
+    })
 
     return () => {
-      waveSurfer.destroy();
-    };
-  }, [audio]);
+      waveSurfer.destroy()
+    }
+  }, [audio])
 
   return (
     <WaveSurferWrap>
       <button
         onClick={() => {
-          waveSurferRef.current.playPause();
-          toggleIsPlaying(waveSurferRef.current.isPlaying());
+          waveSurferRef.current.playPause()
+          toggleIsPlaying(waveSurferRef.current.isPlaying())
         }}
         type="button"
       >
@@ -42,12 +44,12 @@ const Waveform = ({ audio }) => {
       </button>
       <div ref={containerRef} />
     </WaveSurferWrap>
-  );
-};
+  )
+}
 
 Waveform.propTypes = {
   audio: PropTypes.string.isRequired,
-};
+}
 
 const WaveSurferWrap = styled.div`
   display: grid;
@@ -61,7 +63,6 @@ const WaveSurferWrap = styled.div`
     padding: 0;
     background-color: white;
   }
-`;
+`
 
-
-export default Waveform;
+export default Waveform
